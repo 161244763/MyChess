86e66ea5c8ab5826ac75aedbcae5763098a32493
161244763 <92664334+161244763@users.noreply.github.com>
2022-05-20 00:08:27 +0800

    Update board.h

--- MyChess/board.h
+++ MyChess/board.h
@@ -11,11 +11,39 @@ class Board : public QWidget
 public:
     explicit Board(QWidget *parent = nullptr);
     ~Board();
+    void drawStone(QPainter &p);
+    void drawStone(QPainter& painter,int id);
     void paintEvent(QPaintEvent *event);
 //    void drawStone(QPainter& p,int id);
 
+    //返回棋盘行列对应的像素坐标
+    QPoint center(int id);
+    QPoint center(int row,int col);
+
+    //调整下棋轮次
+    void set_redTurn();
+
+    void click(QPoint pt);
+    void click(int id, int row, int col);
+    bool getClickRowCol(QPoint pt,int& row ,int& col);
+    void mouseReleaseEvent(QMouseEvent *);
+
 private:
     Stone* stones[32];
+    int _selectedID = -1;
+    int _clickedID = -1;
+    bool _redTurn = true;
+    float left;
+    float top;
+    float bWidth ;
+    float bHeight;
+    float wWidth ;
+    float wHeight;
+    float wScale ;
+    float bScale ;
+    float gHeight;
+    float gbHeight;
+    float gbWidth;
 signals:
 
 };
